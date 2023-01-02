f33c034725326431ccdfdf3581e32b97a2073228
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2023-01-02 21:05:24 +0100

    Created New bills.component.ts

--- pm-web-app/src/app/bills/bills.component.ts
+++ pm-web-app/src/app/bills/bills.component.ts
@@ -8,10 +8,17 @@ import {HttpClient} from "@angular/common/http";
 })
 export class BillsComponent implements OnInit {
 
-  bill : any;
+  bills : any;
   constructor(private http:HttpClient) {
   }
   ngOnInit(): void {
+    this.http.get("http://localhost:8888/BILLING-SERVICE/bills").subscribe({
+      next : (data)=> {
+        this.bills = data;
+    }, error : (err) => {
+
+      }
+    });
   }
 
 }
