2e94d564baa1f916ecdd7d178a15047b963772b3
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2023-01-02 20:50:50 +0100

    Created New bills.component.ts

--- pm-web-app/src/app/app-routing.module.ts
+++ pm-web-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ProductsComponent} from "./products/products.component";
 import {CustomersComponent} from "./customers/customers.component";
+import {BillsComponent} from "./bills/bills.component";
 
 const routes: Routes = [
   {path : "products", component : ProductsComponent},
-  {path : "customers", component : CustomersComponent}
+  {path : "customers", component : CustomersComponent},
+  {path : "bills", component : BillsComponent}
 ];
 
 @NgModule({
--- pm-web-app/src/app/bills/bills.component.ts
+++ pm-web-app/src/app/bills/bills.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-bills',
   templateUrl: './bills.component.html',
   styleUrls: ['./bills.component.css']
 })
-export class BillsComponent {
+export class BillsComponent implements OnInit {
+
+  bill : any;
+  constructor(private http:HttpClient) {
+  }
+  ngOnInit(): void {
+  }
 
 }
